48ba7c331a67696ccb517b344f4d36c41740cd6b
Sam Lau <esayemm@gmail.com>
2017-07-14 02:49:25 -0700

    default fe port to 5000 so it doesnt collide with node default port

--- packages/jbs-fe/scripts/dev.js
+++ packages/jbs-fe/scripts/dev.js
@@ -5,7 +5,7 @@ const appPaths         = require('../app-paths.js')
 const wpConfig         = require('../configs/webpack.config.js')
 
 module.exports = function webpackDev() {
-  const port = process.env.PORT || 8080
+  const port = process.env.PORT || 5000
 
   wpConfig.entry.app.unshift('webpack-dev-server/client?')
   wpConfig.entry.app.unshift('webpack/hot/dev-server')
