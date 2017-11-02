6a62f96e7294e4a367318b7a41138f81ecc30bd5
Sam Lau <esayemm@gmail.com>
2017-11-02 11:49:08 -0700

    [babel] Using require.resolve need to use package name

--- packages/jbs-base-babel/index.js
+++ packages/jbs-base-babel/index.js
@@ -3,7 +3,7 @@ const BABEL_MODULE = process.env.BABEL_MODULE
 module.exports = {
   "presets": [
     require.resolve('babel-preset-stage-0'),
-    [require.resolve('@babel/env'), {
+    [require.resolve('@babel/preset-env'), {
       modules: ['false', false].includes(BABEL_MODULE) ? false : BABEL_MODULE || 'commonjs',
     }],
   ],
