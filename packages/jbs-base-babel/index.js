7829f438200d87b415d29e20e6a305f932983ea7
Sam Lau <esayemm@gmail.com>
2017-11-02 11:46:10 -0700

    [babel] Switch from es2015 to @babel/preset-env

--- packages/jbs-base-babel/index.js
+++ packages/jbs-base-babel/index.js
@@ -3,13 +3,8 @@ const BABEL_MODULE = process.env.BABEL_MODULE
 module.exports = {
   "presets": [
     require.resolve('babel-preset-stage-0'),
-    [require.resolve('babel-preset-es2015'), {
+    [require.resolve('@babel/env'), {
       modules: ['false', false].includes(BABEL_MODULE) ? false : BABEL_MODULE || 'commonjs',
     }],
-  ],
-  "plugins": [
-    require.resolve('babel-plugin-transform-runtime'),
-    require.resolve('babel-plugin-transform-decorators-legacy'),
-    require.resolve('babel-plugin-transform-class-properties'),
   ],
 }
