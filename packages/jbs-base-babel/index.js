69e2b8b9afcf3208cdb8340f0289fc0a91c8eb13
Sam Lau <esayemm@gmail.com>
2017-11-02 14:52:16 -0700

    [babel] Revert env til its out of beta

--- packages/jbs-base-babel/index.js
+++ packages/jbs-base-babel/index.js
@@ -3,8 +3,13 @@ const BABEL_MODULE = process.env.BABEL_MODULE
 module.exports = {
   "presets": [
     require.resolve('babel-preset-stage-0'),
-    [require.resolve('@babel/preset-env'), {
+    [require.resolve('babel-preset-es2015'), {
       modules: ['false', false].includes(BABEL_MODULE) ? false : BABEL_MODULE || 'commonjs',
     }],
+  ],
+  "plugins": [
+    require.resolve('babel-plugin-transform-runtime'),
+    require.resolve('babel-plugin-transform-decorators-legacy'),
+    require.resolve('babel-plugin-transform-class-properties'),
   ],
 }
